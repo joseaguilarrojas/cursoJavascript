208643cd2ea7e824dd2148ac04e0a4e61d94b7e0
Alvaro Toledo <alvarojavier2020@outlook.es>
2022-10-25 11:08:27 -0500

    push

---
+++ 1.tiposDeDatos/ejercicios.js
@@ -0,0 +1,9 @@
+//averiguar el methodo de javascript que me permite sanes que tipo de dato tengo
+//mediante typeof
+var tipo=typeof "hola"
+var tipo1=typeof 12
+var tipo2=typeof false
+
+console.log(tipo)
+console.log(tipo1)
+console.log(tipo2)
--- 2.variables/ejercicios.js
+++ 2.variables/ejercicios.js
@@ -1,4 +1,8 @@
 //averiguar las diferencias entre "ler" y "var"
+//let
+//se utiliza para declarar variables que estan limitados al alcance de un bloque, o una exprecion, esto quiere decir que solo se considerarár en esa lina o bloque
+//var
+//se utiliza para definir una variable, que a diferencia del "let" es de caracter global o local en una funcion, sin importar el ambito del bloque
 
 //la cocina del codigo
 
