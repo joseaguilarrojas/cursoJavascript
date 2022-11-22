969d6ee5637a1e95907dd65b512f3b18a14050c8
Alvaro Toledo <alvarojavier2020@outlook.es>
2022-11-22 09:45:04 -0500

    que es el dom

---
+++ Dom/ejercicios.js
@@ -0,0 +1,6 @@
+//captura la variable como si fueras un pokenon
+let contenido = document.querySelector('#contenido')
+contenido.textContent = 'hola profe'
+let texto = document.createElement('p')
+contenido.appendChild (texto)
+texto.textContent = 'hola profesor'
